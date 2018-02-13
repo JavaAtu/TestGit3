5dae753c3618ba6d22ad4cbad085a4a8d8550ccf
Artur B <abartosiak@interia.pl>
2018-02-13 23:28:43 +0100

    USUNIECIE IMPLEMENTACJI OBIEKTU Z App

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -1,17 +1,14 @@
 package com.app;
 
-import com.app.model.Person;
-
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        Person p = new Person();
-        p.setName("JAN");
-        p.setAge(30);
+
+
     }
 }
